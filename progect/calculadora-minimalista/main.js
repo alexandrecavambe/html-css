f1d3af3b1533e687edf8e079357f19185147f821
Alexandre Jemex Victorino Cavambe <245668036+alexandrecavambe@users.noreply.github.com>
2026-01-05 20:09:40 +0100

    comit

--- progect/calculadora-minimalista/main.js
+++ progect/calculadora-minimalista/main.js
@@ -1,1 +1,21 @@
-console.log('Hello World!');
+const numero = document.querySelector('#result');
+function insert(num) {
+    numero.value += num
+}
+function clean() {
+    document.querySelector('#result').value = ""
+}
+function back(){
+    const input = document.querySelector('#result')
+    input.value = input.value.substring(0, input.value.length -1)
+}
+function calcular(){
+    const input = document.querySelector('#result')
+    if(input.value){
+        try{
+            input.value = eval(input.value)
+        }catch{
+        input.value = "Expressão Inválida"
+    }
+    }
+}
